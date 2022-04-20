a568b94d23ebb3e037e177324292aa144051fd64
Hafidh Rizky <hafidhrzk@gmail.com>
2022-04-20 13:53:50 +0700

    fourth

--- src/config/api.js
+++ src/config/api.js
@@ -17,3 +17,11 @@ export const API = () => {
     delete: executeAPI,
   };
 };
+
+export const setAuthToken = (token) => {
+  if (token) {
+    API.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+  } else {
+    delete API.defaults.headers.commin['Authorization'];
+  }
+};
