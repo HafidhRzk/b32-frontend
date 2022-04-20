495c0991a32bb6f081bb0d172bce870452315570
Hafidh Rizky <hafidhrzk@gmail.com>
2022-04-20 14:29:07 +0700

    eight

--- src/config/api.js
+++ src/config/api.js
@@ -1,22 +1,30 @@
-export const API = () => {
-  const baseUrl =
-    process.env.REACT_APP_SERVER_URL ||
-    'https://b32-backend-dumbways.herokuapp.com/api/v1' ||
-    'https://localhost:5000/api/v1';
+import axios from 'axios';
 
-  const executeAPI = async (endpoint, config) => {
-    const response = await fetch(baseUrl + endpoint, config);
-    const data = await response.json();
-    return data;
-  };
+export const API = axios.create({
+  baseURL: process.env.REACT_APP_SERVER_URL ||
+  'https://b32-backend-dumbways.herokuapp.com/api/v1' ||
+  'https://localhost:5000/api/v1',
+});
 
-  return {
-    get: executeAPI,
-    post: executeAPI,
-    patch: executeAPI,
-    delete: executeAPI,
-  };
-};
+// export const API = () => {
+//   const baseUrl =
+//     process.env.REACT_APP_SERVER_URL ||
+//     'https://b32-backend-dumbways.herokuapp.com/api/v1' ||
+//     'https://localhost:5000/api/v1';
+
+//   const executeAPI = async (endpoint, config) => {
+//     const response = await fetch(baseUrl + endpoint, config);
+//     const data = await response.json();
+//     return data;
+//   };
+
+//   return {
+//     get: executeAPI,
+//     post: executeAPI,
+//     patch: executeAPI,
+//     delete: executeAPI,
+//   };
+// };
 
 export const setAuthToken = (token) => {
   if (token) {
