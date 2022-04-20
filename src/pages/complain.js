377904d9f58929633044970116dfe7b211def3c8
Hafidh Rizky <hafidhrzk@gmail.com>
2022-04-20 13:48:52 +0700

    third

--- src/pages/complain.js
+++ src/pages/complain.js
@@ -8,8 +8,8 @@ function Complain(){
   
     useEffect(() => {
       socket = io(Process.env.REACT_APP_SERVER_URL || 
-        'https://b32-backend-dumbways.herokuapp.com/api/v1/' || 
-        'http://localhost:5000/api/v1/');
+        'https://b32-backend-dumbways.herokuapp.com/api/v1' || 
+        'http://localhost:5000/api/v1');
   
       return () => {
         socket.disconnect();
